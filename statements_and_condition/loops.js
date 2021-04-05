de1a2333727b841ea2ab005f5bf95049be7ded6d
Neely <neely@Neelys-MBP.HPH.Local>
2021-04-05 07:41:22 -0500

    random numbers

---
+++ statements_and_condition/loops.js
@@ -0,0 +1,23 @@
+// Nando went to the vending machine to buy himself a cookie. The cookie costs $4 dollars. He paid with a $10 bill, the vending machine paid him back in quarters. Write a loop that says how many quarters he got in return.
+
+
+for (let i = 10; i <= 10; i++) {
+    let change = i / 0.25
+    console.log(change);
+}
+
+// console.log(Math.floor(Math.random() * 20));
+
+
+//score = 100;
+// let grade = console.log(`your score of ${score} `);
+
+// if (score >=91 && score <= 100 ){
+//     console.log('your score of ' + score + ' grade is A');
+// } else if( score >= 81 && score <= 90){
+// console.log('your score of ' + score + ' grade is B');
+// } else if(score <= 80 && score >= 71){
+// console.log('your score of ' + score + '  grade is C');
+// }else{
+//     console.log('try harder next time');
+// }
