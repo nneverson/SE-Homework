24fee143106f99048fcd018f4bd99b3cceae301d
Neely <neely@Neelys-MBP.HPH.Local>
2021-05-19 19:34:13 -0500

    create input form

--- hooks-login/src/App.js
+++ hooks-login/src/App.js
@@ -1,12 +1,36 @@
-
-
+import React, { useState } from 'react';
+import LoginForm from './components/LoginForm';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	const adminUser = {
+		email: 'admin@admin.com',
+		password: 'admin123',
+	};
+	const [user, setUser] = useState({ name: '', email: '' });
+	const [error, setError] = useState('');
+
+	const Login = (details) => {
+		console.log(details);
+	};
+
+	const Logout = () => {
+		console.log('logout');
+	};
+
+	return (
+		<div className='App'>
+			{user.email !== '' ? (
+				<div className='welcome'>
+					<h2>
+						welcome <span>{user.name}</span>{' '}
+					</h2>
+					<button>Logout</button>
+				</div>
+			) : (
+				<LoginForm />
+			)}
+		</div>
+	);
 }
 
 export default App;
